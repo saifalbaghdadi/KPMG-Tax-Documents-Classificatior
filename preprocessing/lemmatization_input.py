65de3ab435b7453369b548e51832b8f4c3b73a7f
IrinaSing <irinasingh.mail@yandex.ru>
2022-06-15 11:17:28 +0200

    lammetization single txt

---
+++ preprocessing/lemmatization_input.py
@@ -0,0 +1,36 @@
+import re
+
+#import nltk
+import nltk
+nltk.download("stopwords")
+nltk.download('punkt')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.download('words')
+
+#stop-words
+stop_words_nl=set(nltk.corpus.stopwords.words('dutch'))
+stop_words_en=set(nltk.corpus.stopwords.words('english'))
+
+import spacy
+import en_core_web_sm
+
+nlp = en_core_web_sm.load()
+en_words = set(nltk.corpus.words.words())
+overly_abundant = ["article", "decree", "publication"]
+months = ["january", "february", "march", "april", "may", "june", "july", "august", "september", "october", "november", "december"]
+
+def lemmatizer(text):
+
+        cleaned_text = re.sub(r'[\W\d]', " ", text)
+        cleaned_text = text.replace("\n", " ").strip().replace(";", " ").strip().replace(",", " ").strip().replace(".", " ").strip()
+        doc = nlp(cleaned_text)
+        cleaned_lemmas = [t.lemma_ for t in doc if t.lemma_ not in stop_words_nl 
+                            and t.lemma_ in en_words
+                            and t.lemma_ not in stop_words_en
+                            and t.lemma_.lower() not in overly_abundant
+                            and t.lemma_.lower() not in months
+                            and not re.match(r".*\d.*", t.lemma_)]
+
+        return cleaned_lemmas
+
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,22 @@
 import streamlit as st
 import numpy as np
 import os
+#import sys
+
+# import functions
+#sys.path.insert(0, '/preprocessing/lemmatization.py')
+from preprocessing.lemmatization_input import lemmatizer
+
 st.header("JuridIQ")
 
 # Input text
 txt = st.text_area('Article to analyze', ''' ''')
-# st.write('Sentiment:', run_sentiment_analysis(txt))
+preprocessed_txt =  lemmatizer(txt)
+
 
 # Get the topic
 if st.button('Get the topic'):
-    st.write('Topic:') #displayed when the button is clicked
+    st.write('Lemmas:', preprocessed_txt) #displayed when the button is clicked
 else:
     st.write('') #displayed when the button is unclicked
 
@@ -30,3 +37,4 @@ if st.button('Recommend similar articles'):
     st.write('Articles:') #displayed when the button is clicked
 else:
     st.write('') #displayed when the button is unclicked     
+
