b552dde0d61591c49c8ecef7977e33fd16503d50
IrinaSing <irinasingh.mail@yandex.ru>
2022-06-15 12:05:02 +0200

    input handling

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,18 +5,24 @@ import os
 
 # import functions
 #sys.path.insert(0, '/preprocessing/lemmatization.py')
-from preprocessing.lemmatization_input import lemmatizer
+from input_handling.lemmatization_input import lemmatizer
+from input_handling.translation_input import translate_doc
 
 st.header("JuridIQ")
 
 # Input text
 txt = st.text_area('Article to analyze', ''' ''')
-preprocessed_txt =  lemmatizer(txt)
+
+# function that translates and lemmatizes text
+def eng_lemmas(text):
+    translated_text = translate_doc(txt)
+    lemmatized_txt =  lemmatizer(translated_text)
+    return lemmatized_txt
 
 
-# Get the topic
+# Get the topic (for now - lemmas from translated text)
 if st.button('Get the topic'):
-    st.write('Lemmas:', preprocessed_txt) #displayed when the button is clicked
+    st.write('Lemmas:', eng_lemmas(txt)) #displayed when the button is clicked
 else:
     st.write('') #displayed when the button is unclicked
 
